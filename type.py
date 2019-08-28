56e9d66afe337e410ca7ab61b13d1d194256d065
luoheng <1301089462@qq.com>
2019-08-28 17:30:14 +0800

    gradual type not work

---
+++ type.py
@@ -0,0 +1,14 @@
+
+
+def hello(s: int) -> int:
+    if s > 10:
+        return "10"
+    else:
+        return s
+
+def main():
+    s = 12
+    print(hello(s))
+
+if __name__ == "__main__":
+    main()
