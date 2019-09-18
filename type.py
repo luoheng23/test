ea6d0b0e9069f3078c5f89fb956485ec49be2b8e
luoheng <1301089462@qq.com>
2019-09-18 15:24:24 +0800

    test for display matrix

--- type.py
+++ type.py
@@ -1,6 +1,13 @@
-
-x = [1, 0, 1, 0]
-binStr = "".join(str(i) for i in x)
-print(int(binStr, 2))
-
-print(sum(v*2**i for i, v in enumerate(reversed(x))))
+def main():
+  
+    rows = int(input("What is the upper bound of multiplication table? "))
+    print("The multiplication table for 2 to", rows)
+    print()
+    counter = 0
+    multiplicationTable(rows,counter)
+  
+def multiplicationTable(rows,counter):
+    size = rows + 1
+    for i in range(1,size):
+        print(*(i*nums for nums in range(1, size)), sep='\t')
+main()
