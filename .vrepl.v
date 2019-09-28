fca94c1ec3314ea285620629c1a2897f3d987ea1
luoheng <1301089462@qq.com>
2019-09-28 19:34:02 +0800

    test for v

---
+++ .vrepl.v
@@ -0,0 +1,3 @@
+struct S {x int}
+fn s() S {return S{1} }
+println(s())
--- hello.v
+++ hello.v
@@ -1,5 +1,10 @@
-	ph := p.cgen.add_placeholder()
-	for p.tok == .dot {
-		println(typ_str)
-		typ_str = p.dot(typ_str, ph)
-	}
+
+struct S {
+	x int
+}
+
+fn s() S {
+	return S{1}
+}
+
+println(s())
