744ee4d958df315c1176cc35818df8c289861027
luoheng <1301089462@qq.com>
2019-10-02 19:02:47 +0800

    test for v

--- hello.v
+++ hello.v
@@ -1,20 +1,4 @@
 
-struct Nord {
-	h int
-	w int
-}
-
-fn (n Nord) get() int{
-	return n.h + n.w
-}
-
-struct M {
-	n &Nord
-	w int
-}
-
-fn main() {
-	nt := Nord{2, 3}
-	m := M{n: &nt, w: 3}
-	println(m.n.get())
+pub fn hello() {
+	println("hello world")
 }
