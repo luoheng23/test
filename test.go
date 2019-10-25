17a2419162d4ef645a32afc50edb18c436072f33
luoheng <1301089462@qq.com>
2019-10-25 14:46:06 +0800

    test for sort

--- test.go
+++ test.go
@@ -6,3 +6,9 @@ func main() {
 	t, _ := strconv.Atoi("-2")
 	print(t)
 }
+
+type SortBy []Type
+
+func (a SortBy) Len() int           { return len(a) }
+func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool { return a[i] < a[j] }
