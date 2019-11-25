26f0fd71bea79cec962e2b68cfd5b7eff83bdb74
luoheng <1301089462@qq.com>
2019-11-25 16:16:44 +0800

    test a guess

--- type.py
+++ type.py
@@ -1,5 +1,20 @@
-#   coding=utf-8
- 
-coded, words = open("scrambled-words.txt", "r", encoding="UTF-8"), open("dictionary.txt", encoding="UTF-8")
-key, value = [x.strip('\n') for x in coded], [x.strip('\n') for x in words]
- 
+
+# string = "áéÔàœèùêac"
+# string2 = "e\u0301" == "é"
+# b1 = bytes("e\u0301", encoding="utf8")
+# b2 = bytes("é", encoding="utf8")
+# print(str(b1, "utf8"))
+# print(str(b2, "utf8"))
+# print(len("á"))
+# print(string2)
+# string2 = "ábcdefg"
+# string = "ábcdefg"
+# print(string[0], string2[0])
+# print(len("é"))
+# print(len(string))
+# for c in string:
+#     print(c)
+
+string2 = "ábcdefg"  # this is mine
+string = "ábcdefg"   # this is yours
+print(string[0], string2[0])
