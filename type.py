44af72a7cd954be235a7c343400c5b7af78936f3
luoheng <1301089462@qq.com>
2019-11-25 19:52:28 +0800

    python learning

--- type.py
+++ type.py
@@ -1,20 +1,6 @@
 
-# string = "áéÔàœèùêac"
-# string2 = "e\u0301" == "é"
-# b1 = bytes("e\u0301", encoding="utf8")
-# b2 = bytes("é", encoding="utf8")
-# print(str(b1, "utf8"))
-# print(str(b2, "utf8"))
-# print(len("á"))
-# print(string2)
-# string2 = "ábcdefg"
-# string = "ábcdefg"
-# print(string[0], string2[0])
-# print(len("é"))
-# print(len(string))
-# for c in string:
-#     print(c)
+def add(x: int, y: int) -> int:
+    return x + y
 
-string2 = "ábcdefg"  # this is mine
-string = "ábcdefg"   # this is yours
-print(string[0], string2[0])
+print(add(2, 3))
+print(add("H", "w"))
