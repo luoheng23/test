ebd67d28b6444593f2fa8125758e9577f1cce226
luoheng <1301089462@qq.com>
2020-11-09 14:33:52 +0800

    add test

--- test.c
+++ test.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
-
-int main ()
-{
-   char str1[10];
-   char str2[10];
-   printf("输入第一个字符串：\n");
-   scanf("%s", str1);
-   printf("输入第二个字符串：\n");
-   scanf("%s", str2);
-
-   int i;
-   i=strcmp(str1,str2);
-   if(0==i){
-     printf("两个字符串完全相同" );
-   }
-   else  printf("两个字符串不相同" );
-
-   return 0;
+int main() {
+  unsigned char a = 254;
+  if ((unsigned char)-1 > a) {
+    printf("%d\n", a);
+  }
+  printf("%d %d\n", a, (unsigned char)-1);
 }
