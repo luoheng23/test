135c4d5ee2bb958a4aadac81ec1e705fc8cc582a
luoheng <1301089462@qq.com>
2020-12-11 14:42:51 +0800

    save

--- test.c
+++ test.c
@@ -2,8 +2,13 @@
 
 int main()
 {
+  printf("EOF %d\n", EOF);
   int a = 20;
+  printf("??!\n");
   char *c = (char *)&a;
+  char *message;
+  message = "Hello world";
+  printf("%s\n", message);
   for (int i = 0; i < 4; ++i)
   {
     printf("value: %d\n", (int)c[i]);
