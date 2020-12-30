1f6d6d7bc0dc8022a30a4b452e17a3c03f86faf9
luoheng <1301089462@qq.com>
2020-12-30 16:19:48 +0800

    test

--- test.c
+++ test.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
-int main()
+long long a()
 {
-  int lval = 0xFEDCBA98 << 32;
-  printf("%d\n", lval);
+  long long sum = 0;
+  for (long long i = 0; i < 2000000; ++i)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+long long b()
+{
+  long long sum = 0;
+  for (long long i = 0; i < 1000000; ++i)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+long long c()
+{
+  long long sum = 0;
+  for (long long i = 0; i < 3000000; ++i)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+long long main()
+{
+  long long z = 0;
+  for (long long i = 0; i < 1000; ++i)
+  {
+    z += a();
+    z += b();
+    z += c();
+  }
+  printf("%d\n", z);
+  return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+
+// do nothing
+void Loop()
+{
+    int count = 0;
+    for(; count < 200000000; ++count);
+}
+
+// call Loop inside
+void Loop1()
+{
+    int count = 0;
+    for(; count < 600000000; ++count);
+
+    Loop();
+}
+
+void Loop2()
+{
+    int count = 0;
+    for(; count < 200000000; ++count);
+}
+
 int main()
 {
-    const int &p = 5;
-    cout << p << endl;
+    Loop1();
+    Loop2();
+    Loop2();
+
+    return 0;
 }
